8e62b3165c2fd5dba7398690927dc2ee48ed2b6e
George H. <gzh@alumni.princeton.edu>
2025-05-08 19:06:31 -0400

    colorful toasts

--- src/app/trade/page.tsx
+++ src/app/trade/page.tsx
@@ -3,7 +3,6 @@
 import TradePanel from "@/components/TradePanel";
 import NavBar from "@/components/NavBar";
 import Trades from "@/components/Trades";
-import { Toaster } from "sonner";
 import { useState } from "react";
 import dynamic from 'next/dynamic';
 import AuthModal from "@/components/AuthModal";
@@ -20,7 +19,6 @@ export default function TradePage() {
   return (
     <div className="min-h-screen flex flex-col">
       <NavBar onAuthChange = {setAuthOpen} />
-      <Toaster richColors />
 
       <main className="flex-1 grid grid-cols-1 xl:grid-cols-[1fr_380px] gap-6 p-4 max-w-7xl mx-auto w-full">
         {/* TradingView chart */}
